8b95c824ac857096f518e25e0b44a23e3cf2b805
mikeplavsky <mike.plavsky@quest.com>
2015-09-19 22:42:59 +0300

    updated dependencies to newer versions

--- src/leiningen/new/figwheel_node/project.clj
+++ src/leiningen/new/figwheel_node/project.clj
@@ -2,12 +2,12 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3190"]
-                 [figwheel "0.2.5-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [figwheel "0.4.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-figwheel "0.2.5-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.4.0"]]
 
   :source-paths ["src"]
 
